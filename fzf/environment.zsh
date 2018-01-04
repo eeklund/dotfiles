a56bba2ed65a062ace7ff5b125ea426f7c9032c9
Ian Whitney <whit0694@umn.edu>
2018-01-03 20:03:20 -0600

    Add fzf & ripgrep
    Starting point for these changes is
    
    https://medium.com/@crashybang/supercharge-vim-with-fzf-and-ripgrep-d4661fc853d2
    
    Some differences
    
    - brew to install fzf
    - removing the `--ignore-hidden` flag from rg usage. I'm ignoring those
    files for a reason, thanks.
    - setting up rg autocompletion. fzf autocompletion is managed by the
    brew formula

---
+++ fzf/environment.zsh
@@ -0,0 +1,5 @@
+# --files: List files that would be searched but do not search
+# --hidden: Search hidden files and folders
+# --follow: Follow symlinks
+# --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
