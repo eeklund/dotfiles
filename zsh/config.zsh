b051d047f6fc520c5340be94a7d04a8880e8c310
Ian Whitney <whit0694@umn.edu>
2018-01-04 21:03:07 -0600

    Fix hub autocompletion
    https://github.com/github/hub/issues/363
    
    Removing this setting allows `g pu<tab>` to auto-complete to `g pull-request`,
    as I'd expect

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,10 +15,6 @@ setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
