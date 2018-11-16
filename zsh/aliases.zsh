45d4580d2cf840ea79b1dca81b93654d73f424b3
Ian Whitney <eeklund@umn.edu>
2018-11-16 00:48:01 -0600

    Update initial values to my user

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,14 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias catalogs="ssh whit0694@web-mgmt01.oit.umn.edu"
+alias catalogs="ssh eeklund@web-mgmt01.oit.umn.edu"
 alias shop="vim ~/Dropbox/family/shopping.txt"
-alias ansible-playbook="ansible-playbook --user=whit0694"
+alias ansible-playbook="ansible-playbook --user=eeklund"
 
 alias gc_sdpm="git clone https://github.umn.edu/sdp/sdp_management.git"
 
 alias caffeinate="caffeinate -d -i"
 
-export STUB_INTERNET_ID="whit0694"
+export STUB_INTERNET_ID="eeklund"
 
 alias vacation="echo 'umn.edu_gjh72idh4stdgi3q2173d5e6jo@group.calendar.google.com' | pbcopy"
